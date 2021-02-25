13cb567ed2b37f0f6c97f9e18e76b1454986f2e2
Socheat Sok <alex@socheat.net>
2021-02-25 15:05:55 +0700

    feat(core): add generator.js and prompts.js

---
+++ generator.js
@@ -0,0 +1,4 @@
+module.exports = api => {
+    // Generator stub
+    // https://cli.vuejs.org/dev-guide/plugin-dev.html#generator
+}
---
+++ prompts.js
@@ -0,0 +1,4 @@
+module.exports = pkg => {
+    // Prompts stub
+    // https://cli.vuejs.org/dev-guide/plugin-dev.html#prompts
+}
