e45c5dc54b6f7c15889fd50095dba9305aac30a1
Socheat Sok <alex@socheat.net>
2021-03-01 15:03:32 +0700

    feat(core): add extendPackage scripts

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,11 @@
 module.exports = api => {
+    // Install additional dependencies
+    api.extendPackage({
+        scripts: {
+            'bump:release': 'yarn version',
+            'prebump:release': 'yarn lint && yarn test:unit --ci'
+        },
+    })
+
     api.render('./template')
 }
