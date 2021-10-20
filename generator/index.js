2fba8813f8f0394752451821387dda81ebe5a3a2
Socheat Sok <alex@socheat.net>
2021-10-20 15:14:38 +0700

    feat(generator): add node engine version

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,9 @@
 module.exports = api => {
     // Install additional dependencies
     api.extendPackage({
+        engines: {
+            node: ">=14.0.0 <=16"
+        },
         scripts: {
             'bump:release': 'yarn version',
             'prebump:release': 'yarn lint && yarn test:unit --ci'
