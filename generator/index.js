42f128fbe96343a30dadf3a90de5efc0b5658387
Socheat Sok <alex@socheat.net>
2021-10-21 16:14:56 +0700

    feat(generator): add exit log

--- generator/index.js
+++ generator/index.js
@@ -3,4 +3,9 @@ const package = require('./packages')
 module.exports = api => {
   api.extendPackage(package)
   api.render('./template')
+
+  // adapted from https://github.com/vuetifyjs/vue-cli-plugins/blob/master/packages/vue-cli-plugin-vuetify/generator/index.js
+  api.onCreateComplete(() => {
+    api.exitLog('Github: https://github.com/soramitsukhmer/vue-cli-plugin-presets')
+  })
 }
